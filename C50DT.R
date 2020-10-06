621a854af5f1d725ed461156ea8bc55e4fc3986c
Kenechukwu <kenechukwu.ajufo@gmail.com>
2020-10-06 15:59:24 +0100

    Add Project Report and Code Artefact

---
+++ C50DT.R
@@ -0,0 +1,24 @@
+library(C50)
+library(printr)
+library(data.table)
+library(tree)
+
+students.df <- read.csv(file.choose())
+students.df$Final.Grade = as.factor(students.df$Final.Grade)
+
+#Split iris data into train and test
+train.indices <- sample(1:nrow(students.df), 100)
+students.df.train <- students.df[train.indices, ]
+students.df.test <- students.df[-train.indices, ]
+
+#Train decision tree
+model <- C5.0(Final.Grade ~., data=students.df.train)
+
+#Test
+results <- predict(object=model, newdata=students.df.test, type="class")
+
+#Look at confusion matrix
+table(results, students.df.test$Final.Grade)
+
+#Plot the tree
+plot(model)
